d9289eb644926f7fc37b3116f1031db267a4dd37
Danny Robinson <daniel@djrobinson.me>
2018-07-18 07:45:36 -0600

    Unit tests for deltas

--- server/integrations/test/Bittrex.test.js
+++ server/integrations/test/Bittrex.test.js
@@ -1,26 +1,55 @@
 const assert = require('assert');
 const Bittrex = require('../Bittrex');
 
-beforeEach(() => {
-  console.log("Testing tests?");
-});
 
 describe('Bittrex integration tests', () => {
-  it('Is working at all', () => {
-    assert.equal(true, true);
+  const exchange = new Bittrex();
+
+
+  const orderDeltaBid = { Z: [{ R: 0.0002, Q: 1000 }], S: []};
+  const orderDeltaAsk = { Z: [], S: [{ R: 0.0003, Q: 1000 }]};
+  const markets = [{ MarketName: 'BTC-ETH', LogoUrl: 'test.com' }, { MarketName: 'BTC-OMG', LogoUrl: 'test.com' }]
+
+
+  const expectedBidUpdate = {
+    type: 'BID_UPDATE',
+    rateString: 'bittrex0.0002',
+    rate: 0.0002,
+    amount: 1000
+  };
+  const expectedAskUpdate = {
+    type: 'ASK_UPDATE',
+    rateString: 'bittrex0.0003',
+    rate: 0.0003,
+    amount: 1000
+  };
+  const expectedMarkets = [{ market: 'BTC-ETH', logo: 'test.com' }, { market: 'BTC-OMG', logo: 'test.com' }];
+
+
+  it('Parse bids into correct structure', () => {
+    let parsedMarkets;
+    exchange.emitOrderBook = (res) => {
+      console.log("Override emit orderbook");
+      parsedMarkets = res;
+    };
+    exchange.parseOrderDelta('ORDER_DELTA', orderDeltaBid);
+    assert.deepEqual(parsedMarkets, expectedBidUpdate);
+  });
+
+  it('Parses asks into correct structure', () => {
+    let parsedMarkets;
+    exchange.emitOrderBook = (res) => {
+      console.log("Override emit orderbook");
+      parsedMarkets = res;
+    };
+    exchange.parseOrderDelta('ORDER_DELTA', orderDeltaAsk);
+    assert.deepEqual(parsedMarkets, expectedAskUpdate);
+  });
+
+
+  it('Parse markets matches correct structure', () => {
+    const parsedMarkets = exchange.parseMarkets(markets);
+    assert.deepEqual(parsedMarkets, expectedMarkets);
   });
 });
 
-// {
-//   type: 'BID_UPDATE',
-//   rateString: 'bittrex0.0002',
-//   rate: 0.002,
-//   amount: 1000
-// }
-
-// {
-//   type: 'ASK_UPDATE',
-//   rateString: 'bittrex0.0003',
-//   rate: 0.003,
-//   amount: 1000
-// }
--- server/integrations/test/Poloniex.test.js
+++ server/integrations/test/Poloniex.test.js
@@ -1,40 +1,50 @@
 const assert = require('assert');
 const Poloniex = require('../Poloniex')
 
-// let exchange;
-
-// beforeEach(() => {
-
-//   console.log("Testing tests?");
-// });
-
 describe('Poloniex integrations test', () => {
   const exchange = new Poloniex();
+
   const markets = { 'BTC_ETH': 0, 'BTC_OMG': 0}
-  const parsedMarkets = exchange.parseMarkets(markets);
+  const bidDelta = JSON.stringify(['14', '379823752', [['o', 1, 0.0002, 1000]]]);
+  const askDelta = JSON.stringify(['14', '273598723', [['o', 0, 0.0003, 1000]]]);
+
   const expectedMarkets = [{ market: 'BTC-ETH' }, { market: 'BTC-OMG' }];
+  const expectedBidUpdate = {
+    type: 'BID_UPDATE',
+    rateString: 'poloniex0.0002',
+    rate: 0.0002,
+    amount: 1000
+  };
+  const expectedAskUpdate = {
+    type: 'ASK_UPDATE',
+    rateString: 'poloniex0.0003',
+    rate: 0.0003,
+    amount: 1000
+  };
+
+  it('Parse bids into correct structure', () => {
+    let parsedMarkets;
+    exchange.emitOrderBook = (res) => {
+      console.log("Override emit orderbook");
+      parsedMarkets = res;
+    };
+    exchange.parseOrderDelta(bidDelta);
+    assert.deepEqual(parsedMarkets, expectedBidUpdate);
+  });
+
+  it('Parses asks into correct structure', () => {
+    let parsedMarkets;
+    exchange.emitOrderBook = (res) => {
+      console.log("Override emit orderbook");
+      parsedMarkets = res;
+    };
+    exchange.parseOrderDelta(askDelta);
+    assert.deepEqual(parsedMarkets, expectedAskUpdate);
+  });
+
   it('Parse markets matches correct structure', () => {
+    const parsedMarkets = exchange.parseMarkets(markets);
     assert.deepEqual(parsedMarkets, expectedMarkets);
   });
 });
 
-
-// INIT OBJECT
-// data[2][0][1].orderBook[1]
-
-// UPDATE OBJECT
-// data[2]
-
-// {
-//   type: 'BID_UPDATE',
-//   rateString: 'poloniex0.0002',
-//   rate: 0.002,
-//   amount: 1000
-// }
-
-// {
-//   type: 'ASK_UPDATE',
-//   rateString: 'poloniex0.0003',
-//   rate: 0.003,
-//   amount: 1000
-// }
