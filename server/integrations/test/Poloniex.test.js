1a1f329eea5dd90106157181cdc63c85d921666a
Danny Robinson <daniel@djrobinson.me>
2018-07-18 08:01:49 -0600

    Init order book parse complete

--- server/integrations/test/Poloniex.test.js
+++ server/integrations/test/Poloniex.test.js
@@ -7,6 +7,7 @@ describe('Poloniex integrations test', () => {
   const markets = { 'BTC_ETH': 0, 'BTC_OMG': 0}
   const bidDelta = JSON.stringify(['14', '379823752', [['o', 1, 0.0002, 1000]]]);
   const askDelta = JSON.stringify(['14', '273598723', [['o', 0, 0.0003, 1000]]]);
+  const initBook = JSON.stringify(['14', '985923849', [['35', { orderBook: [{'0.0003': '1000'}, {'0.0002': '2000'}]}]]])
 
   const expectedMarkets = [{ market: 'BTC-ETH' }, { market: 'BTC-OMG' }];
   const expectedBidUpdate = {
@@ -21,25 +22,50 @@ describe('Poloniex integrations test', () => {
     rate: 0.0003,
     amount: 1000
   };
+  const expectedInitMarket = {
+    type: 'ORDER_BOOK_INIT',
+    bids: {
+      'poloniex0.0002': {
+        rate: 0.0002,
+        amount: 2000,
+        exchange: 'poloniex'
+      }
+    },
+    asks: {
+      'poloniex0.0003': {
+        rate: 0.0003,
+        amount: 1000,
+        exchange: 'poloniex'
+      }
+    }
+  }
 
-  it('Parse bids into correct structure', () => {
-    let parsedMarkets;
+  it('Parses initial orderbook into correct structure', () => {
+    let parsed;
     exchange.emitOrderBook = (res) => {
       console.log("Override emit orderbook");
-      parsedMarkets = res;
+      parsed = res;
+    };
+    exchange.parseOrderDelta(initBook);
+    assert.deepEqual(parsed, expectedInitMarket);
+  })
+
+  it('Parse bids into correct structure', () => {
+    let parsed;
+    exchange.emitOrderBook = (res) => {
+      parsed = res;
     };
     exchange.parseOrderDelta(bidDelta);
-    assert.deepEqual(parsedMarkets, expectedBidUpdate);
+    assert.deepEqual(parsed, expectedBidUpdate);
   });
 
   it('Parses asks into correct structure', () => {
-    let parsedMarkets;
+    let parsed;
     exchange.emitOrderBook = (res) => {
-      console.log("Override emit orderbook");
-      parsedMarkets = res;
+      parsed = res;
     };
     exchange.parseOrderDelta(askDelta);
-    assert.deepEqual(parsedMarkets, expectedAskUpdate);
+    assert.deepEqual(parsed, expectedAskUpdate);
   });
 
   it('Parse markets matches correct structure', () => {
